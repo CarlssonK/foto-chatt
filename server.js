dc2c5903e6134fb63ceea67504e7c63c3b16e5e5
Oliver Carlsson <oliver@framecore.se>
2021-05-14 13:20:08 +0200

    basic layout + ssn

--- server.js
+++ server.js
@@ -4,13 +4,75 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
-// app.use("/", (req, res) => {
-//     res.send("HELLO SERVER");
-// });
-
+// Post new message in global chat
 app.post("/api/global", (req, res) => {
-    console.log(req.body);
+    let message = req.body;
+
+    // set timestamp
+    message.timestamp = Date.now();
+
+    // message all open client with new message
+    broadcast("new-message", message);
+
+    res.send("ok");
 });
+
+// List of connections
+let connections = [];
+
+// Route for SSE
+app.get("/api/sse", (req, res) => {
+    // // Add the response to open connections
+    connections.push(res);
+
+    console.log("NEW USER");
+
+    // Remove connection on close
+    req.on("close", () => {
+        connections = connections.filter((openRes) => openRes != res);
+
+        // message all open connections that a client disconnected
+        broadcast("disconnect", {
+            message: "client disconnected",
+        });
+    });
+
+    // Set headers to mark that this is SSE
+    // and that we don't close the connection
+    res.set({
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+    });
+
+    // message all connected clients that this
+    // client connected
+    broadcast("connect", {
+        message: "clients connected: " + connections.length,
+    });
+});
+
+// function to send message to all connected clients
+function broadcast(event, data) {
+    // loop through all open connections and send
+    // some data without closing the connection (res.write)
+    for (let res of connections) {
+        // syntax for a SSE message: 'event: message \ndata: "the-message" \n\n'
+        res.write("event:" + event + "\ndata:" + JSON.stringify(data) + "\n\n");
+    }
+}
+
+// Generate Random User ID
+function userId() {
+    let result = "";
+    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let charactersLength = characters.length;
+    for (let i = 0; i < 6; i++) {
+        result += characters.charAt(
+            Math.floor(Math.random() * charactersLength)
+        );
+    }
+    return result;
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,76 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useStates } from "react-easier";
 // ###########################################################
 // Temporär page. Funktionaliteten här kommer vara i Chat.jsx
 // ###########################################################
 
 function Home() {
+    const input = useRef(null);
+
+    const s = useStates({
+        input: "",
+    });
+
+    useEffect(() => {
+        startSSE();
+    }, []);
+
+    const startSSE = () => {
+        let sse = new EventSource("/api/sse");
+
+        sse.addEventListener("connect", (message) => {
+            let data = JSON.parse(message.data);
+            console.log("[connect]", data);
+        });
+
+        sse.addEventListener("disconnect", (message) => {
+            let data = JSON.parse(message.data);
+            console.log("[disconnect]", data);
+        });
+
+        sse.addEventListener("new-message", (message) => {
+            let data = JSON.parse(message.data);
+            console.log("[new-message]", data);
+        });
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
+        if (s.input === "") return;
+        const message = s.input;
+        s.input = "";
+        input.current.value = "";
 
-        const res = await fetch("http://localhost:3000/api/global", {
+        await fetch("http://localhost:3000/api/global", {
             method: "POST",
             headers: {
                 Accept: "application/json",
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ msg: "hello" }),
+            body: JSON.stringify({ msg: message }),
         });
-        const data = await res.json();
-        console.log(data);
+    };
+
+    const handleInput = (e) => {
+        s.input = e.target.value;
     };
 
     return (
-        <>
-            <h2>Home Page</h2>
-            <form onSubmit={handleSubmit}>
-                <input type="text" />
-            </form>
-        </>
+        <div className="home">
+            <div>Home Page</div>
+            <div className="main">
+                <ul>
+                    <li>msg: hasdhashd</li>
+                    <li>msg: hasdhashd2</li>
+                    <li>msg: hasdhashd123123</li>
+                </ul>
+            </div>
+            <div className="bottom">
+                <form onSubmit={handleSubmit}>
+                    <input type="text" ref={input} onChange={handleInput} />
+                </form>
+            </div>
+        </div>
     );
 }
 
