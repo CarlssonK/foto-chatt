51d28313e301120ca91ef14a063f9fb060b1490f
orka299 <you@example.com>
2021-05-16 16:47:56 +0200

    sign in logic working

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,28 +1,42 @@
-import React, {useRef} from "react";
-import {useAuth} from "../contexts/AuthContext"
+import React, { useRef, useState } from "react";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function Signup() {
-const emailRef = useRef();
-const passwordRef = useRef();
-const passwordConfirmRef = useRef();
-//const {Signup} = useAuth();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const { signup } = useAuth();
 
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState("");
 
+  
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords dosen't match");
+    }
+    try {
+      setError("");
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      setError("404");
+    }
+    setLoading(false);
+  }
 
-
-    return (
-        <div>
-           
-
-            Signup
-            <input type="text" name="" id="" ref={emailRef} />
-            <input type="text" name="" id="" ref={passwordRef}/>
-            <input type="text" name="" id="" ref={passwordConfirmRef}/>
-            <button>Sign up</button>
-            
-           
-        </div>
-    );
+  return (
+    <div>
+      Signup
+      
+      <form onSubmit = {handleSubmit}>
+          {error && <h1>{error}</h1>}
+      <input type="email" name="" id="" ref={emailRef} required />
+      <input type="password" name="" id="" ref={passwordRef} required />
+      <input type="password" name="" id="" ref={passwordConfirmRef} required />
+      <button disabled = {loading}>Sign up</button>
+      </form>
+    </div>
+  );
 }
-
-
