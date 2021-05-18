095c8fd099cdd12263849fee8624431e0cff811c
matildastr <matilda.stridsman@gmail.com>
2021-05-18 11:54:54 +0200

    Länkar i topbar klara

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import styles from '../styles/Topbar.module.css'
-
+import { useLocation, useHistory } from 'react-router-dom'
 
 function Topbar() {
+    const loc = useLocation()
+    const history = useHistory()
     return (
+        <div>
         <div className={styles.topbar}>
-             <button className={styles.btn}><i className="material-icons">arrow_back</i></button>
+             <button className={styles.btn}>{loc.pathname != '/' ? (
+             <i onClick={history.goBack} className="material-icons">arrow_back</i>
+             ) : ''}</button>
             <h4>hej</h4>
-            <button className={styles.btn}><i className="material-icons">manage_accounts</i></button>
+            <button className={styles.btn}><i><a href="/Profile" className="material-icons">manage_accounts</a></i></button>
+        </div>
         </div>
     )
 }
