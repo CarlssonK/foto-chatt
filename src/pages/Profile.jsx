59e17c80fb2d66cdc0283c7d6595991cd96fc6c6
dirren17 <71495689+dirren17@users.noreply.github.com>
2021-05-19 08:50:14 +0200

    Profile changes

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,39 @@
 import React from "react";
+import Topbar from "../components/Topbar"
+import styles from "../styles/Profile.module.css"
+
 
 function Profile() {
     return (
-        <div>
-            Profile
-            <div>name</div>
+        <div className={styles.profileContainer}>
+            <Topbar />
+            
+            <input className={styles.loginDetails} 
+                placeholder="Username"
+                disabled
+            />
+            
+            <input className={styles.loginDetails}
+                placeholder="Password"
+                disabled
+            />
+
+            <div className={styles.bottomContainer}>
+
+                
+                {/* {<Switch checked={state.gilad} onChange={handleChange} name="notification" />}
+
+                <label className="switch">
+                <input type="checkbox">
+                <span className="slider round"></span>
+                </label> */}
+            
+
+                <div className={styles.settings}>NOTIFIKATIONER</div>
+                <div className={styles.settings}>GPS</div>
+                <div className={styles.settings}>ANNAT</div>
+
+            </div>
         </div>
     );
 }
