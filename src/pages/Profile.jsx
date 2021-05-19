4193a5769198c95c4f8bc15db82912efeb38df05
dirren17 <71495689+dirren17@users.noreply.github.com>
2021-05-19 16:34:40 +0200

    Profile changes

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,26 +6,26 @@ import { useAuth } from "../contexts/AuthContext";
 
 function Profile() {
     const { currentUser} = useAuth();
+    
     return (
         <div className={styles.profileContainer}>
             <Topbar chatName={"Profile"} />
             
-            <input className={styles.loginDetails} 
-                placeholder="Username"
-                disabled
-            />
-            
-            <input className={styles.loginDetails}
-                placeholder="Password"
-                disabled
-            />
+            <div className={styles.loginDetails}>
+            Username
+            </div>
+
+            <div className={styles.loginDetails}>
+            E-mail
+            </div>
+
 
             <div className={styles.bottomContainer}>
             
 
-                <div className={styles.settings}>NOTIFIKATIONER</div>
+                <div className={styles.settings}>Notifikationer</div>
                 <div className={styles.settings}>GPS</div>
-                <div className={styles.settings}>ANNAT</div>
+                <div className={styles.settings}>Annat</div>
 
             </div>
 
